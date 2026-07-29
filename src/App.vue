d7025908baccc7163a9d058ee45a649c3864a6d4
Dmitriy S <onlyzoran@gmail.com>
2026-07-29 11:44:02 +0200

    League refactoring

--- src/App.vue
+++ src/App.vue
@@ -6,29 +6,25 @@ import serieAData from '@/data/serie-a-26-27.json'
 import laLigaData from '@/data/la-liga-26-27.json'
 import bundesligaData from '@/data/bundesliga-26-27.json'
 
-const eplTeams = eplData.map((entry, index) => ({
-  id: String(index + 1),
-  name: entry.team,
-  winProbability: entry.win_predict,
-}))
+interface LeagueEntry {
+  team: string
+  win_predict: number
+}
 
-const serieATeams = serieAData.map((entry, index) => ({
-  id: String(index + 1),
-  name: entry.team,
-  winProbability: entry.win_predict,
-}))
+function toTeams(entries: LeagueEntry[]) {
+  return entries.map((entry, index) => ({
+    id: String(index + 1),
+    name: entry.team,
+    winProbability: entry.win_predict,
+  }))
+}
 
-const laLigaTeams = laLigaData.map((entry, index) => ({
-  id: String(index + 1),
-  name: entry.team,
-  winProbability: entry.win_predict,
-}))
-
-const bundesligaTeams = bundesligaData.map((entry, index) => ({
-  id: String(index + 1),
-  name: entry.team,
-  winProbability: entry.win_predict,
-}))
+const leagues = [
+  { title: 'EPL 26/27', teams: toTeams(eplData) },
+  { title: 'Serie A 26/27', teams: toTeams(serieAData) },
+  { title: 'La Liga 26/27', teams: toTeams(laLigaData) },
+  { title: 'Bundesliga 26/27', teams: toTeams(bundesligaData) },
+]
 </script>
 
 <template>
@@ -36,21 +32,10 @@ const bundesligaTeams = bundesligaData.map((entry, index) => ({
     <AppHeader />
     <main class="flex-1 flex items-start justify-center pt-14 gap-4">
       <TeamProbabilityList
-        title="EPL 26/27"
-        :teams="eplTeams"
-        class="flex flex-col "
-      />
-      <TeamProbabilityList
-        title="Serie A 26/27"
-        :teams="serieATeams"
-      />
-      <TeamProbabilityList
-        title="La Liga 26/27"
-        :teams="laLigaTeams"
-      />
-      <TeamProbabilityList
-        title="Bundesliga 26/27"
-        :teams="bundesligaTeams"
+        v-for="league in leagues"
+        :key="league.title"
+        :title="league.title"
+        :teams="league.teams"
       />
     </main>
   </div>
